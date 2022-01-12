bbd11a9861a32e6bd9b70426581392421c85b38a
downkhg <downkhg@gmail.com>
2022-01-12 19:46:01 +0900

    c# 기초문법 템플릿

---
+++ Unity2D/UnityBasic/UnityBasic/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace UnityBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //ValMain();
+            //CriticalAttackMain();
+            StageMain();
+        }
+
+        static void ValMain()
+        {
+            int nScore = 0;
+            float fRat = 1.0f / 4.0f;
+            Console.WriteLine("Score:" + nScore);
+            Console.WriteLine("Rat:" + fRat);
+        }
+
+        static void CriticalAttackMain()
+        {
+            Console.WriteLine("CriticalAttackMain");
+        }
+
+        static void StageMain()
+        {
+
+        }
+
+        static void AttackWhile()
+        {
+
+        }
+
+        static void MonsterListMain()
+        {
+
+        }
+    }
+}
