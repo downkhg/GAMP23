e5a41ef2c387ac3cb6daf351239c1b9eb39fe14a
downkhg <downkhg@gmail.com>
2022-01-14 19:30:01 +0900

    리스트,절차게임제작,객체지향,포켓몬

---
+++ Unity2D/UnityBasic/UnityBasic/PokemonGame.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityBasic
+{
+    class Pokemon
+    {
+        string strName;
+        int nAttack;
+        int nHP;
+        public Pokemon(string name, int atk, int hp)
+        {
+            StrName = name;
+            nAttack = atk;
+            nHP = hp;
+        }
+
+        public string StrName { get => strName; set => strName = value; }
+
+        public void Attack(Pokemon target)
+        {
+            target.nHP -= this.nAttack;
+        }
+        public bool Death()
+        {
+            if (nHP > 0) return false;
+            else return true;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("###" + StrName + "###");
+            Console.WriteLine("HP:" + nHP);
+            Console.WriteLine("Atk:" + nAttack);
+        }
+    }
+
+    class Tranner
+    {
+        List<Pokemon> pokemons;
+
+        public void Catch(Pokemon pokemon)
+        {
+            pokemons.Add(pokemon);
+        }
+
+        public Pokemon Throw(string name)
+        {
+            Pokemon returnPokemon = pokemons.Find(pokemon => pokemon.StrName ==  name);
+            if (returnPokemon != null) pokemons.Remove(returnPokemon); 
+            return returnPokemon;
+        }
+    }
+
+    class PokemonGame
+    {
+    }
+}
--- Unity2D/UnityBasic/UnityBasic/Program.cs
+++ Unity2D/UnityBasic/UnityBasic/Program.cs
@@ -1,9 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UnityBasic
 {
+    class Player
+    {
+        string strName;
+        int nAttack;
+        int nHP;
+        public Player(string name,int atk, int hp)
+        {
+            strName = name;
+            nAttack = atk;
+            nHP = hp;
+        }
+        public void Attack(Player target)
+        {
+            target.nHP -= this.nAttack;
+        }
+        public bool Death()
+        {
+            if (nHP > 0) return false;
+            else return true;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("###" + strName + "###");
+            Console.WriteLine("HP:" + nHP);
+            Console.WriteLine("Atk:" + nAttack);
+        }
+    }
+
     class Program
     {
+        static void ClassBattleMain()
+        {
+            Player player = new Player("player",10,100);
+            Player monster = new Player("slime",5,20);
+
+            List<Player> listMonster = new List<Player>();
+            listMonster.Add(monster);
+            listMonster.Add(new Player("skeleton", 10, 50));
+            listMonster.Add(new Player("zombie", 10, 50));
+            listMonster.Add(new Player("dragon", 20, 200));
+
+            while (true)
+            {
+                if (player.Death() == false)
+                {
+                    player.Attack(monster);
+                    monster.Show();
+                }
+                else break;
+
+                if (monster.Death() == false)
+                {
+                    monster.Attack(player);
+                    player.Show();
+                }
+                else break;
+            }
+        }
+
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
@@ -11,7 +70,9 @@ namespace UnityBasic
             //PlayerAttackMonsterMain();
             //CriticalAttackMain();
             //StageMain();
-            AttackWhile();
+            //ttackWhile();
+            //BattleMain();
+            ClassBattleMain();
         }
 
         static void ValMain()
@@ -97,10 +158,79 @@ namespace UnityBasic
                 Console.WriteLine("2.PlayerAttack:" + nPlayerAttack);
             }
         }
+        //플레이어가 몬스터를 공격한다.
+        //(플레이어의 공격력:정수)만큼 (몬스터의 hp:정수)가 감소한다.
+        //몬스터가 플레이어에게 반격한다.
+        //(몬스터의 공격력:정수)만큼 (플레이어의 hp:정수)가 감소한다.
+        //둘중하나가 죽을때까지 반복한다. -> 둘중하나가 죽으면 끝난다.
+        //몬스터가 죽었지만 플레이어를 공격해서 플레이어가 사망했다 -> 죽으면 공격할수없다.
+        static void BattleMain()
+        {
+            int nPlayerHP = 100;
+            int nPlayerAttack = 10;
+            int nMonsterHP = 100;
+            int nMonsterAttack = 10;
 
+            Random cRandom = new Random();
+
+            Console.WriteLine("1.MonsterHP:" + nMonsterHP);
+            Console.WriteLine("1.MonsterAttack:" + nMonsterAttack);
+
+            Console.WriteLine("1.PlayerHP:" + nPlayerHP);
+            Console.WriteLine("1.PlayerAttack:" + nPlayerAttack);
+            //판단이 어려울땐 고민하지말고, 결과의 유효성을 적어서 검증한다.
+            //10 > 0 -> 참  || 0 > 0 -> 거짓 -> 참
+            //10 > 0 -> 참  && 0 > 0 -> 거짓 -> 거짓
+            while (nPlayerHP > 0 && nMonsterHP > 0)
+            {
+                //if (nPlayerHP <= 0 || nMonsterHP <= 0) break;
+
+                if (nPlayerHP > 0)
+                {
+                    //플레이어가 공격한다.
+                    Console.WriteLine("Player Attack!");
+                    
+                    int nRandom = cRandom.Next(4);//ex) Next(4),Next(0,3)-0,1,3 / Next(1,3) - 1,2
+                    Console.WriteLine("Rondom:" + nRandom);
+                    if (nRandom == 1)
+                    {
+                        nMonsterHP = nMonsterHP - nPlayerAttack * 2;
+                        Console.WriteLine("Critical!!!");
+                    }
+                    else
+                    {
+                        nMonsterHP = nMonsterHP - nPlayerAttack;
+                    }
+                    Console.WriteLine("2.MonsterHP:" + nMonsterHP);
+                    Console.WriteLine("2.PlayerAttack:" + nPlayerAttack);
+                }
+                //else break;
+
+                if (nMonsterHP > 0)
+                {
+                    //몬스터가 반격한다.
+                    Console.WriteLine("Monster Attack!");
+                    nPlayerHP = nPlayerHP - nMonsterAttack;
+                    Console.WriteLine("2.PlayerHP:" + nPlayerHP);
+                    Console.WriteLine("2.MonsterAttack:" + nMonsterAttack);
+                }
+                //else break;
+            }
+        }
+        //몬스터의 리스트를 만들고, 첫번째값과 마지막값을 출력하고, 모든 리스트의 값을 출력한다.(단, 슬라임,스켈레톤,좀비,드래곤)
         static void MonsterListMain()
         {
+            List<string> listMonsters = new List<string>();
+            listMonsters.Add("Slime");
+            listMonsters.Add("Skeleton");
+            listMonsters.Add("Zombie");
+            listMonsters.Add("Dragon");
 
+            Console.WriteLine(listMonsters[0]);
+            Console.WriteLine(listMonsters[3]);
+
+            for (int i = 0; i < listMonsters.Count; i++)
+                Console.WriteLine(i + ":" + listMonsters[i]);
         }
     }
 }
