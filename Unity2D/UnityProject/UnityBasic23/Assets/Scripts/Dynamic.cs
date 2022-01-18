370220a0a9c38981581afd655a5a55394554b508
downkhg <downkhg@gmail.com>
2022-01-18 19:47:43 +0900

    플레이어 이동 및 물리

--- Unity2D/UnityProject/UnityBasic23/Assets/Scripts/Dynamic.cs
+++ Unity2D/UnityProject/UnityBasic23/Assets/Scripts/Dynamic.cs
@@ -12,7 +12,15 @@ public class Dynamic : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        transform.position += Vector3.right;
+    {   
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += Vector3.right * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position += Vector3.left * Time.deltaTime;
+        //상하이동하기
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.position += Vector3.up * Time.deltaTime;
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.position += Vector3.down * Time.deltaTime;
     }
 }
