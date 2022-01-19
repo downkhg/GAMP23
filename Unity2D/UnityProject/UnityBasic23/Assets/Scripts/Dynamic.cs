a40b4ecb5aad48e231fa967dd17f5bd389e2b260
downkhg <downkhg@gmail.com>
2022-01-19 19:47:35 +0900

    점프와 충돌체크

--- Unity2D/UnityProject/UnityBasic23/Assets/Scripts/Dynamic.cs
+++ Unity2D/UnityProject/UnityBasic23/Assets/Scripts/Dynamic.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Dynamic : MonoBehaviour
 {
+    public float JumpPower;
+    public bool isGround;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +19,26 @@ public class Dynamic : MonoBehaviour
             transform.position += Vector3.right * Time.deltaTime;
         if (Input.GetKey(KeyCode.LeftArrow))
             transform.position += Vector3.left * Time.deltaTime;
-        //상하이동하기
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.position += Vector3.up * Time.deltaTime;
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.position += Vector3.down * Time.deltaTime;
+        //if (Input.GetKey(KeyCode.Space))
+        //    transform.position += Vector3.up * 3 * Time.deltaTime;
+        //스페이스키를 눌렀을때 점프
+        //점프: 위로 힘을 가해서 올라가다 떨어지는것
+        //발판이있어야 점프가 가능하다 -> 땅에 서있을때 점프가 가능하다.
+        //점프중일때는 점프가 되지않아야한다.
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGround == true)
+            {
+                Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+                rigidbody.AddForce(Vector3.up * JumpPower);
+                isGround = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+        Debug.Log("OnCollisionEnter2D:"+collision.gameObject.name);
     }
 }
