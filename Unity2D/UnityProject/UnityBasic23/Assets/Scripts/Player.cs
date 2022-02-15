6408f4dbb770c4afc1ec7eb8147e9f008328f307
downkhg <downkhg@gmail.com>
2022-02-15 19:44:09 +0900

    능력치 추가하기

---
+++ Unity2D/UnityProject/UnityBasic23/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int nAttack;
+    public int nHP;
+
+    public void Attack(Player target)
+    {
+        target.nHP -= this.nAttack;
+    }
+    public bool Death()
+    {
+        if (nHP > 0) return false;
+        else return true;
+    }
+
+    public void Show()
+    {
+        Debug.Log("###" + this.gameObject.name + "###");
+        Debug.Log("HP:" + nHP);
+        Debug.Log("Atk:" + nAttack);
+    }
+    public int idx = 0;
+    private void OnGUI()
+    {
+        int w = 100, h = 20;
+        
+        GUI.Box(new Rect(w * idx,0,w,h),"###" + this.gameObject.name + "###");
+        GUI.Box(new Rect(w * idx, 20, w, h), "HP:" + nHP);
+        GUI.Box(new Rect(w * idx, 40, w, h), "Atk:" + nAttack);
+    }
+}
