982ed002d975b760a0797abc141e34b029eb2015
downkhg <downkhg@gmail.com>
2022-05-06 18:50:31 +0900

    벡터 그림대로 코드 작성하기

--- ProgramningBasic/DataStruct/4.STL/4.STL/4.STL.cpp
+++ ProgramningBasic/DataStruct/4.STL/4.STL/4.STL.cpp
@@ -9,47 +9,111 @@ using namespace std;
 //3.배열의 크기를 런타임중에 변경가능하다.
 void VectorMain()
 {
-	vector<int> contair(1);
-	contair[0] = 10;
+	vector<int> container(1);
+	container[0] = 10;
 	cout << "Print:";
-	for (int i = 0; i < contair.size(); i++)
-		printf("[%d]%d,",i,contair[i]);//cout << "[" << i << "]" << contair[i] << ",";
+	for (int i = 0; i < container.size(); i++)
+		printf("[%d]%d,",i,container[i]);//cout << "[" << i << "]" << container[i] << ",";
 	printf("\n");//cout << endl;
-	contair.resize(3);
+	container.resize(3);
 	cout << "Print:";
-	for (int i = 0; i < contair.size(); i++)
-		printf("[%d]%d,", i, contair[i]);//cout << "[" << i << "]" << contair[i] << ",";
+	for (int i = 0; i < container.size(); i++)
+		printf("[%d]%d,", i, container[i]);//cout << "[" << i << "]" << container[i] << ",";
 	printf("\n");//cout << endl;
 	//1.추가 2.삽입 3.삭제
-	contair.push_back(100);//추가
+	container.push_back(100);//추가
 	vector<int>::iterator it;//반복자
-	for (it = contair.begin(); it != contair.end(); it++)
+	for (it = container.begin(); it != container.end(); it++)
 		cout << "[" << &*it << "]" << *it << ",";
 	cout << endl;
-	it = contair.begin();
-	contair.insert(it, 110);//삽입
-	for (it = contair.begin(); it != contair.end(); it++)
+	it = container.begin();
+	container.insert(it, 110);//삽입
+	for (it = container.begin(); it != container.end(); it++)
 		cout << "[" << &*it << "]" << *it << ",";
 	cout << endl;
-	it = find(contair.begin(), contair.end(), 10);
-	if (it != contair.end())
+	it = find(container.begin(), container.end(), 10);
+	if (it != container.end())
 		cout << "Find:" << *it << endl;
-	contair.erase(it);//삭제
-	for (it = contair.begin(); it != contair.end(); it++)
+	container.erase(it);//삭제
+	for (it = container.begin(); it != container.end(); it++)
 		cout << "[" << &*it << "]" << *it << ",";
 	cout << endl;
-	contair.clear();
-	for (it = contair.begin(); it != contair.end(); it++)
+	container.clear();
+	for (it = container.begin(); it != container.end(); it++)
 		cout << "[" << &*it << "]" << *it << ",";
 	cout << endl;
 }
-
+//1.생성 10개
+//2.크기변성 5개
+//3.0,-10추가
+//4.찾기 70
+//5.삽입 70에 -20삽입 -> 80은 위에서 찾기를 했을때 70이므로 80이 될수는 없다.
+//6.지우기 삽입된 노드(반복자:이터레이터) 삭제
+//7.뒤집기,모두 삭제
 void VectorTestMain()
 {
 	//슬라이드6~10까지 그림과 같도록 만들어보기.
+	vector<int> container(10);
+	cout << "1.Init(" << container.size() << "):" << container.capacity() << endl;
+	for (int i = 0; i < container.size(); i++)
+		container[i] = 100 - (i * 10);
+	for (int i = 0; i < container.size(); i++)
+		cout << &container[i] << "[" << i << "]"<< container[i] <<",";
+	cout << endl;
+
+	container.resize(5);
+	cout << "2.resize(" << container.size() << "):" << container.capacity() << endl;
+	for (int i = 0; i < container.size(); i++)
+		cout << &container[i] << "[" << i << "]" << container[i] << ",";
+	cout << endl;
+
+	container.push_back(0);
+	cout << "3-1.push_back(0)" << endl;
+	for (int i = 0; i < container.size(); i++)
+		cout << &container[i] << "[" << i << "]" << container[i] << ",";
+	cout << endl;
+
+	container.push_back(-10);
+	cout << "3-2.push_back(-10)" << endl;
+	for (int i = 0; i < container.size(); i++)
+		cout << &container[i] << "[" << i << "]" << container[i] << ",";
+	cout << endl;
+
+	vector<int>::iterator itFind = find(container.begin(), container.end(), 70);
+	if (itFind != container.end())
+		cout << "4.Find[" << &*itFind << "]:" << *itFind << endl;
+
+	vector<int>::iterator itInsert = container.insert(itFind, -20);
+	if (itInsert != container.end())
+		cout << "5-1.itInsert[" << &*itInsert << "]:" << *itInsert << endl;
+	cout << "5-2.itInsert(" << container.size() << ")" << endl;
+	for (int i = 0; i < container.size(); i++)
+		cout << &container[i] << "[" << i << "]" << container[i] << ",";
+	cout << endl;
+	/*if (itFind != container.end())
+		cout << "5-3.Find[" << &*itFind << "]:" << *itFind << endl;*/
+
+	container.erase(itInsert);
+	cout << "6.erase("<<container.size()<<")" << endl;
+	for (int i = 0; i < container.size(); i++)
+		cout << &container[i] << "[" << i << "]" << container[i] << ",";
+	cout << endl;
+
+	reverse(container.begin(), container.end());
+	cout << "7-1.reverse(" << container.size() << ")" << endl;
+	for (int i = 0; i < container.size(); i++)
+		cout << &container[i] << "[" << i << "]" << container[i] << ",";
+	cout << endl;
+
+	container.clear();
+	cout << "7-2.clear(" << container.size() << ")" << endl;
+	for (int i = 0; i < container.size(); i++)
+		cout << &container[i] << "[" << i << "]" << container[i] << ",";
+	cout << endl;
 }
 
 void main()
 {
-	VectorMain();
+	//VectorMain();
+	VectorTestMain();
 }
