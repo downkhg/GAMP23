5888a14bf764fd403564d3686f020e504a9a9b65
downkhg <downkhg@gmail.com>
2022-05-18 18:57:16 +0900

    객체의 생성 및 소멸, 전역,지역,매개,정적지역변수,동적할당.

--- ProgramningBasic/3.C++/2.CarClass/2.CarClass/2.CarClass.cpp
+++ ProgramningBasic/3.C++/2.CarClass/2.CarClass/2.CarClass.cpp
@@ -43,26 +43,46 @@ void CarDashBoard(SCar& cCar)
 	cout << "Gear:" << cCar.eGear << endl;
 }
 
-void StructCarTestMain()
-{
-	SCar sCar;
-	CarInit(sCar, "red");
-	CarDashBoard(sCar);
-	CarSetGear(sCar, SCar::E_GEAR::D);
-	CarDashBoard(sCar);
-	CarAccel(sCar);
-	CarDashBoard(sCar);
-	CarBreak(sCar);
-	CarDashBoard(sCar);
-}
-
 class CCar //클래스: 함수와 변수를 묶어서 나타낸것(캡슐화)
 {
 public://클래스외부에서 함수의 접근을 허용한다.
 	//자동차의 조작은 도둑이 타서도 조작하면 자동차는 움직일수있다.
 	//멤버함수: 멤버함수는 외부에서 변수를 변경하기 위해서 사용하는 메세지.
 	enum E_GEAR { P, R, N, D };
-
+	//생성자: 객체(변수)가 생성될때 호출 되는 함수. 
+	//생성자도 함수이므로 매개변수의 갯수나 타입이 다르면 오버로딩 가능하다.
+	//CCar()//기본생성자: 기본멤버들을 기본값으로 초기화하는 생성자.
+	//{
+	//	eGear = E_GEAR::N;
+	//	nSpeed = 0;
+	//	strColor = "none";
+	//	cout << "DefaultCar(" << eGear << "," << nSpeed << "," << strColor <<")" << endl;
+	//}
+	//CCar(string color)//매개변수를 통해 객체 내부의 값을 생성시 변경 가능한 생성자.
+	//{
+	//	eGear = E_GEAR::N;
+	//	nSpeed = 0;
+	//	strColor = color;
+	//	cout << "PrameterCar(" << eGear << "," << nSpeed << "," << strColor << ")" << endl;
+	//}
+	CCar(string color = "gray")//디폴트매개변수를 활용하여 생성자 1개로 기본생성자도 대처함.
+	{
+		eGear = E_GEAR::N;
+		nSpeed = 0;
+		strColor = color;
+		cout << "DefaultPrameterCar["<<this<<"](" << eGear << "," << nSpeed << "," << strColor << ")" << endl;
+	}
+	//소멸자: 객체(변수)가 소멸될때 호출되는 함수.
+	~CCar()
+	{
+		cout << "~CCar[" << this << "](" << eGear << "," << nSpeed << "," << strColor << ")" << endl;
+	}
+	//복사생성자: 객체를 복사할때 호출되는 함수
+	CCar(CCar& car)
+	{
+		*this = car;
+		cout << "CopyCCar[" << this << "](" << eGear << "," << nSpeed << "," << strColor << ")" << endl;
+	}
 	void Init(string color)
 	{
 		eGear = E_GEAR::N;
@@ -119,8 +139,9 @@ void StructCarTestMain()
 
 void ClassCarTestMain()
 {
-	CCar cCar;
-	cCar.Init("red");
+	cout << "ClassCarTestMain Start" << endl;
+	CCar cCar;//차량생산시에 도색을 제외하고 작업을 마침.
+	cCar.Init("red"); //주문자가 주문한 색상으로 도색을 완료함.
 	//은닉: 멤버변수를 외부에서 함부로 변경할수 없도록 만드는것.
 	//cCar.strColor = "blue"; //길을 지나가던 누군가가 내차의 페인트칠을 했다. -> 범법행위
 	cCar.SetColor("blue"); //내 허락을 받아 자동차 색상을 변경한다 -> 불법아님.
@@ -130,10 +151,98 @@ void ClassCarTestMain()
 	cCar.DashBoard();
 	cCar.Break();
 	cCar.DashBoard();
+
+	CCar cCarA("blue");//자동차를 생산할때 파란색으로 도색을 한다.
+	cCarA.SetColor("green"); //내 허락을 받아 자동차 색상을 변경한다 -> 불법아님.
+	cCarA.SetGear(CCar::E_GEAR::D);
+	cCarA.DashBoard();
+	cCarA.Accel();
+	cCarA.DashBoard();
+	cCarA.Break();
+	cCarA.DashBoard();
+	cout << "ClassCarTestMain End" << endl;
 }
 
+CCar g_cCar;//전역변수와 객체의 생성 및 소멸.
+
+void CarSwapVar(CCar a, CCar b)
+{
+	cout << "CarSwapVar(" << &a << "," << &b << ")"<<endl;
+	CCar temp = a;
+	a = b;
+	b = temp;
+}
+
+void CarSwapRef(CCar& a, CCar& b)
+{
+	cout << "CarSwapRef(" << &a << "," << &b << ")" << endl;
+	CCar temp = a;
+	a = b;
+	b = temp;
+}
+
+void CarSwapPtr(CCar* a, CCar* b)
+{
+	cout << "CarSwapPtr(" << a << "," << b << ")" << endl;
+	CCar temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void CarSwapTestMain()
+{
+	cout << "CarSwapTestMain Start" << endl;
+	CCar cCarBlue("blue");
+	CCar cCarRed("red");
+	cCarBlue.DashBoard();
+	cCarRed.DashBoard();
+	CarSwapVar(cCarBlue, cCarRed);
+	cCarBlue.DashBoard();
+	cCarRed.DashBoard();
+	CarSwapRef(cCarBlue, cCarRed);
+	cCarBlue.DashBoard();
+	cCarRed.DashBoard();
+	CarSwapPtr(&cCarBlue, &cCarRed);
+	cCarBlue.DashBoard();
+	cCarRed.DashBoard();
+	cout << "CarSwapTestMain End" << endl;
+}
+
+void StaticLocalCar()
+{
+	cout << "StaticLocalCar Start" << endl;
+	static CCar cCar;
+	cout << "StaticLocalCar End" << endl;
+}
+
+void StaticLocalCarTestMain()
+{
+	cout << "StaticLocalCarTestMain Start" << endl;
+	for (int i = 0; i < 3; i++)
+		StaticLocalCar();
+	cout << "StaticLocalCarTestMain End" << endl;
+}
+
+void DynamicAllocateTestMain()
+{
+	cout << "DynamicAllocateTestMain 1" << endl;
+	CCar* pCar = NULL;
+	cout << "DynamicAllocateTestMain 2" << endl;
+	pCar = new CCar();
+	delete pCar;
+	cout << "DynamicAllocateTestMain 3" << endl;
+	pCar = new CCar[3];
+	delete[] pCar;
+	cout << "DynamicAllocateTestMain 4" << endl;
+}
 
 void main()
 {
-	StructCarTestMain();
+	cout << "main Start" << endl;
+	//StructCarTestMain();
+	//ClassCarTestMain(); //지역변수 객체의 생성과 소멸
+	//CarSwapTestMain(); //매개변수의 객체 생성 및 소멸, 참조자와 포인터는 객체가 아니다.
+	//StaticLocalCarTestMain(); //정적지역 객체의 생성 및 소멸
+	DynamicAllocateTestMain(); //동적할당 객체
+	cout << "main End" << endl;
 }
