771be4335d75c25a72a02e7af30200f1a865fd58
downkhg <downkhg@gmail.com>
2022-06-03 18:50:31 +0900

    상속과 다형성과 가상함수

--- ProgramningBasic/3.C++/7.Polymorphism/7.Polymorphism/7.Polymorphism.cpp
+++ ProgramningBasic/3.C++/7.Polymorphism/7.Polymorphism/7.Polymorphism.cpp
@@ -69,8 +69,8 @@ namespace Inheritance
 		Vector vB;
 		Vector vC;
 	public:
-		TriAngle(Vector a = Vector(), Vector b = Vector(), Vector c = Vector()) { cout << "TriAngle[" << this << "]" << endl; };
-		~TriAngle() { cout << "TriAngle[" << this << "]" << sizeof(*this) << endl; };
+		TriAngle(Vector a = Vector(), Vector b = Vector(), Vector c = Vector()) { cout << "TriAngle[" << this << "]" << sizeof(*this) << endl; };
+		~TriAngle() { cout << "TriAngle[" << this << "]"  << endl; };
 		void Draw()
 		{
 			cout << "TriAngle[" << this << "]::Draw()" << endl;
@@ -126,37 +126,197 @@ namespace Inheritance
 		//for (int i = 0; i < 3; i++)
 		//	(Shape*)arrShape[i]->Draw();
 		((Circle*)arrShape[0])->Draw();
-		((RectAangle*)(arrShape[0]))->Draw();
-		((TriAngle*)(arrShape[0]))->Draw();
+		((RectAangle*)(arrShape[1]))->Draw();
+		((TriAngle*)(arrShape[2]))->Draw();
 	}
 	void PowerPointTestMain()
 	{
+		cout << "#### Inheritance::PowerPointTestMain Start #####" << endl;
 		list<Shape*> listShapes;
-
 		listShapes.push_back(new RectAangle());
 		listShapes.push_back(new TriAngle());
 		listShapes.push_back(new Circle());
-
+		cout << "#### Inheritance::PowerPointTestMain Draw All List #####" << endl;
 		list<Shape*>::iterator it = listShapes.begin();
 		for (it = listShapes.begin(); it != listShapes.end(); it++)
 		{
 			Shape* pShaep = *it;
 			pShaep->Draw();
 		}
-
+		cout << "#### Inheritance::PowerPointTestMain Casting Draw #####" << endl;
 		it = listShapes.begin(); 
 		((RectAangle*)*it)->Draw();
 		it++;
 		((TriAngle*)*it)->Draw();
 		it++;
 		((Circle*)*it)->Draw();
-
-
+		cout << "#### Inheritance::PowerPointTestMain Delete List  #####" << endl;
 		for (it = listShapes.begin(); it != listShapes.end(); it++)
 		{
 			Shape* pShaep = *it;
 			delete pShaep;
 		}
+		listShapes.clear();
+		cout << "#### Inheritance::PowerPointTestMain End #####" << endl;
+	}
+}
+
+namespace Virtual
+{
+	//1차원이라면 객체로 볼수없다.
+	struct Vector//객체: 벡터(2차원 위치)
+	{
+		//public:
+		float x;
+		float y;
+		Vector(float x = 0, float y = 0)
+		{
+			this->x = x;
+			this->y = y;
+		}
+		void Print(const char* msg)
+		{
+			cout << msg << "(" << x << "," << y << ")" << endl;
+		}
+	};
+
+	class Shape //도형:
+	{
+	public:
+		Shape()
+		{
+			cout << "Shape[" << this << "]" << sizeof(*this) << endl;
+		}
+		~Shape()
+		{
+			cout << "~Shape[" << this << "]" << endl;
+		}
+		virtual void Draw()//가상함수: 자식객체에서 부모객체의 함수를 오버라이딩하여 구현하는 것.
+		{
+			cout << "Shape[" << this << "]::Draw()" << endl;
+		};
+	};
+	//원과 같이 삼각형,사각형을 클래스로 만들기
+	class Circle : public Shape
+	{
+		Vector vPos;
+		float fRadius;
+	public:
+		Circle(Vector pos = Vector(), float rad = 1)
+		{
+			cout << "Circle[" << this << "]" << sizeof(*this) << endl;
+			vPos = pos;
+			fRadius = rad;
+
+			cout << "Rad:" << fRadius << endl;
+		}
+		~Circle()
+		{
+			cout << "~Circle[" << this << "]()" << endl;
+		}
+		void Draw() override //부모의 가상함수를 오버라이딩하여 자식객체에서 생성한것. //키워드는 필수사항이 아님.
+		{
+			cout << "Circle[" << this << "]::Draw()" << endl;
+			vPos.Print("Pos");
+			cout << "Rad:" << fRadius << endl;
+		}
+	};
+	class TriAngle : public Shape
+	{
+		Vector vA;
+		Vector vB;
+		Vector vC;
+	public:
+		TriAngle(Vector a = Vector(), Vector b = Vector(), Vector c = Vector()) { cout << "TriAngle[" << this << "]" << sizeof(*this) << endl; };
+		~TriAngle() { cout << "TriAngle[" << this << "]"  << endl; };
+		void Draw()
+		{
+			cout << "TriAngle[" << this << "]::Draw()" << endl;
+			vA.Print("A:");
+			vB.Print("B:");
+			vC.Print("C:");
+		}
+	};
+	class RectAangle : public Shape
+	{
+		Vector vTL;
+		Vector vBR;
+	public:
+		RectAangle(Vector tl = Vector(), Vector br = Vector())
+		{
+			cout << "RectAangle[" << this << "]" << sizeof(*this) << endl;
+			vTL = tl;
+			vBR = br;
+		}
+		~RectAangle()
+		{
+			cout << "~RectAangle[" << this << "]()" << endl;
+		}
+		void Draw()
+		{
+			cout << "RectAangle[" << this << "]::Draw()" << endl;
+			vTL.Print("TopLeft:");
+			vBR.Print("BottomRight:");
+		}
+	};
+
+	//부모의 부분과 자식의 부분은 메모리가 다르다. 
+	//결국 상속을 통해서 두개의 같은이름의 함수를 가진다.
+	void ShapeMain()
+	{
+		Shape cShape;
+		Circle cCrilce({ 1,1 }, 1);
+		cShape.Draw();
+		cShape.Shape::Draw();
+		cCrilce.Draw();
+		cCrilce.Shape::Draw();
+	}
+	//프로그램 실행중에 선택하여 모양을 여러개를 그리려면 어떻게해야하는가?//동적으로 메모리를 할당받아야한다.
+	void RuntimeShapeDrawMain()
+	{
+		void** arrShape;
+		arrShape = new void* [3];
+
+		arrShape[0] = (void*)new Circle();
+		arrShape[1] = (void*)new RectAangle();
+		arrShape[2] = (void*)new TriAngle();
+
+		//for (int i = 0; i < 3; i++)
+		//	(Shape*)arrShape[i]->Draw();
+		((Circle*)arrShape[0])->Draw();
+		((RectAangle*)(arrShape[1]))->Draw();
+		((TriAngle*)(arrShape[2]))->Draw();
+	}
+	//다형성은 가상함수를 이용하여 부모객체에서 자식객체에 멤버에 접근한다.
+	void PowerPointTestMain()
+	{
+		cout << "#### Virtual::PowerPointTestMain Start #####" << endl;
+		list<Shape*> listShapes;
+		listShapes.push_back(new RectAangle());
+		listShapes.push_back(new TriAngle());
+		listShapes.push_back(new Circle());
+		cout << "#### Virtual::PowerPointTestMain Draw All List #####" << endl;
+		list<Shape*>::iterator it = listShapes.begin();
+		for (it = listShapes.begin(); it != listShapes.end(); it++)
+		{
+			Shape* pShaep = *it;
+			pShaep->Draw(); //v-ptr이 생성되고 객체에 담겨있는 주소(각 함수의 오버로딩함수)가 다르다.
+		}
+		cout << "#### Virtual::PowerPointTestMain Casting Draw #####" << endl;
+		it = listShapes.begin();
+		((RectAangle*)*it)->Draw();
+		it++;
+		((TriAngle*)*it)->Draw();
+		it++;
+		((Circle*)*it)->Draw();
+		cout << "#### Virtual::PowerPointTestMain Delete List  #####" << endl;
+		for (it = listShapes.begin(); it != listShapes.end(); it++)
+		{
+			Shape* pShaep = *it;
+			delete pShaep;
+		}
+		listShapes.clear();
+		cout << "#### Inheritance::PowerPointTestMain End #####" << endl;
 	}
 }
 
@@ -164,4 +324,5 @@ void main()
 {
 	//Inheritance::ShapeMain();
 	Inheritance::PowerPointTestMain();
+	Virtual::PowerPointTestMain();
 }
