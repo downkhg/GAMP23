57fba3e06ef93c33cfbfb9cb60801d666697ff97
downkhg <downkhg@gmail.com>
2022-06-20 19:46:37 +0900

    레벨업 추가하기

--- GameProgramming/TextRPG/TextRPG/TextRPG.cpp
+++ GameProgramming/TextRPG/TextRPG/TextRPG.cpp
@@ -3,41 +3,110 @@
 
 using namespace std;
 
+struct Status {
+	int nHP;
+	int nMP;
+	int nStr;
+	int nInt;
+	int nDef;
+
+	Status(int _hp = 0, int _mp = 0, int _str = 0, int _int = 0, int _def = 0)
+	{
+		nHP = _hp;
+		nMP = _mp;
+		nStr = _str;
+		nInt = _int;
+		nDef = _def;
+	}
+	void operator+=(const int value)
+	{
+		this->nHP += value;
+		this->nMP += value;
+		this->nStr += value;
+		this->nDef += value;
+		this->nInt += value;
+	}
+	Status operator+(const Status& status)
+	{
+		Status sResult;
+		sResult.nHP = nHP + status.nHP;
+		sResult.nMP = nMP + status.nMP;
+		sResult.nStr = nStr + status.nStr;
+		sResult.nDef = nDef + status.nDef;
+		sResult.nInt = nInt + status.nInt;
+		return sResult;
+	}
+	Status operator-(const Status& status)
+	{
+		Status sResult;
+		sResult.nHP = nHP - status.nHP;
+		sResult.nMP = nMP - status.nMP;
+		sResult.nStr = nStr - status.nStr;
+		sResult.nDef = nDef - status.nDef;
+		sResult.nInt = nInt - status.nInt;
+		return sResult;
+	}
+	void Show()
+	{
+		cout << "HP:" << nHP << endl;
+		cout << "MP:" << nMP << endl;
+		cout << "STR:" << nStr << endl;
+		cout << "INT:" << nInt << endl;
+		cout << "DEF:" << nDef << endl;
+	}
+};
+
 class Player {
 	string m_strName;
+	Status m_sStatus;
+	int m_nLv;
+	int m_nExp;
 
-	int m_nHP;
-	int m_nMP;
-	int m_nStr;
-	int m_nInt;
-	int m_nDef;
 public:
-	Player(string strName = "none", int _hp = 100, int _mp = 100, int _str = 20, int _int = 10, int _def = 10)
+	Player(string strName = "none", int _hp = 100, int _mp = 100, int _str = 20, int _int = 10, int _def = 10, int _exp = 0)
 	{
-		Set(strName, _hp, _mp, _str, _int, _def);
+		Set(strName, _hp, _mp, _str, _int, _def, _exp);
 	}
 
-	void Set(string strName, int _hp, int _mp, int _str, int _int, int _def)
+	void Set(string strName, int _hp, int _mp, int _str, int _int, int _def, int _exp)
 	{
 		m_strName = strName;
-		m_nHP = _hp;
-		m_nMP = _mp;
-		m_nStr = _str;
-		m_nInt = _int;
-		m_nDef = _def;
+		m_sStatus.nHP = _hp;
+		m_sStatus.nMP = _mp;
+		m_sStatus.nStr = _str;
+		m_sStatus.nInt = _int;
+		m_sStatus.nDef = _def;
+		m_nExp = _exp;
+		m_nLv = 1;
 	}
 
-	void Attack(Player& target)
+	void Attack(Player& taget)
 	{
-		target.Demaged(m_nStr);
+		taget.m_sStatus.nHP -= m_sStatus.nStr - m_sStatus.nDef;
 	}
-	void Demaged(int dem)
+
+	void StillExp(Player& target)
 	{
-		m_nHP -= dem - m_nDef;
+		//타겟의 경험치를 가져와서 내 경험치에 올린다.
+		this->m_nExp = target.m_nLv * 100 + target.m_nExp;
 	}
+
+	bool LvUp()
+	{
+		//만약 경험치가 100 이상되면, 레벨+1, 모든 능력치 10증가, 경험치 초기화.
+		if (m_nExp >= 100)
+		{
+			m_sStatus += 10;
+			m_nLv++;
+			m_nExp -= 100;
+			return true;
+		}
+		return false;
+	}
+
 	bool Dead()
 	{
-		if (m_nHP <= 0)
+		if (m_sStatus.nHP <= 0)
 			return true;
 		return false;
 	}
@@ -45,11 +114,7 @@ public:
 	void Show()
 	{
 		cout << "######### " << m_strName << "######### " << endl;
-		cout << "HP:" << m_nHP << endl;
-		cout << "MP:" << m_nMP << endl;
-		cout << "STR:" << m_nStr << endl;
-		cout << "INT:" << m_nInt << endl;
-		cout << "DEF:" << m_nDef << endl;
+		m_sStatus.Show();
 	}
 };
 
@@ -66,9 +131,26 @@ void main()
 			cPlayer.Attack(cMonster);
 			cMonster.Show();
 		}
+		else
+		{
+			cout << "Monster Win !!" << endl;
+			cMonster.StillExp(cPlayer);
+			if (cMonster.LvUp())
+				cout << "Lv Up!!" << endl;
+			cMonster.Show();
+		}
+
 		if (!cMonster.Dead())
 		{
 			cMonster.Attack(cPlayer);
+			cPlayer.Show();
+		}
+		else
+		{
+			cout << "Player Win !!" << endl;
+			cPlayer.StillExp(cMonster);
+			if (cPlayer.LvUp())
+				cout << "Lv Up!!" << endl;
 			cPlayer.Show();
 		}
 	}
